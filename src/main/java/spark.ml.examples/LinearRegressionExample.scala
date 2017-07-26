1afd1bd9b713441f7848ccf50f9d8fc846697c21
Naresh Korvi <Naresh@Nareshs-MacBook-Pro.local>
2017-07-25 20:34:35 -0400

    Adding Linear Regression Model Example

---
+++ src/main/java/spark.ml.examples/LinearRegressionExample.scala
@@ -0,0 +1,46 @@
+package spark.ml.examples
+
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.sql.SparkSession
+
+object LinearRegressionExample {
+
+  def main(): Unit = {
+
+    // Creating Spark Session
+    val spark = SparkSession.builder().appName("Linear Regression Example").getOrCreate()
+
+    //Loading csv data
+    val csvData = spark.read.option("header", "true").option("inferSchema", "true").csv("")
+
+    //Dataframe for ML takes in this format ("label", "features")
+    import org.apache.spark.ml.feature.VectorAssembler
+
+
+    val df = csvData.select(csvData("Price").as("label"), csvData("Avg Area Income"), csvData("Avg Area House Age"), csvData("Avg Area Number of Rooms"),
+      csvData("Avg Area Number of Bedrooms"), csvData("Area Population"))
+
+    //This returns vector assembler object
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("Avg Area Income", "Avg Area House Age", "Avg Area Number of Rooms",
+        "Avg Area Number of Bedrooms", "Area Population"))
+      .setOutputCol("features")
+
+    val output = assembler.transform(df).select("label", "features")
+
+    println(s"Output: ${output.show()}")
+
+    //Linear Regression Object
+    val lr = new LinearRegression()
+
+    //Fit the model
+    val lrModel = lr.fit(output)
+
+    val trainingSummarry = lrModel.summary
+    trainingSummarry.residuals.show()
+    println(s"TrainingSummary : ${trainingSummarry.predictions.show()}")
+    println(s"RMSE : ${trainingSummarry.rootMeanSquaredError}")
+
+  }
+
+}
